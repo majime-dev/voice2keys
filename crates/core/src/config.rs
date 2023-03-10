2510a496510b28d4a558f7dc836aeb6a6388e761
Akutō Gushiken <akuto@majime.dev>
2023-03-10 06:35:06 +0100

    refactor: use serde to deserialize the sequence

--- crates/core/src/config.rs
+++ crates/core/src/config.rs
@@ -1,7 +1,7 @@
 use anyhow::{bail, Context, Result};
 use config::Config as ConfigRepo;
 use input::Vk;
-use serde::Deserialize;
+use serde::{de::IntoDeserializer, Deserialize, Deserializer};
 use std::collections::HashMap;
 use std::time;
 
@@ -18,19 +18,12 @@ pub struct Timing {
     pub key_hold_duration: time::Duration,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Deserialize)]
 // Config represents a fully-formed, validated configuration.
 pub struct Config {
     // A map of voice commands to a sequence of keys to be pressed.
+    #[serde(deserialize_with = "parse_sequence")]
     pub commands: HashMap<String, Vec<Vk>>,
-    pub timing: Timing,
-}
-
-#[derive(Debug, Deserialize)]
-// RawConfig needs to have its key sequences parsed.
-struct RawConfig {
-    // A map of voice commands to a sequence of keys to be pressed.
-    pub commands: HashMap<String, String>,
     #[serde(default)]
     pub timing: Timing,
 }
@@ -39,126 +32,41 @@ impl Config {
     pub fn new(path: String) -> Result<Self> {
         log::debug!("Loading config from {}", path);
 
-        let raw = ConfigRepo::builder()
+        let config = ConfigRepo::builder()
             .add_source(config::File::with_name(path.as_str()))
             .build()
             .with_context(|| format!("failed to read config from {}", path))?
-            .try_deserialize::<RawConfig>()
+            .try_deserialize::<Config>()
             .with_context(|| format!("failed to parse config from {}", path))?;
 
-        log::debug!("Loaded config from {}: {:#?}", path, raw);
+        log::debug!("Loaded config from {}: {:#?}", path, config);
 
-        // Let's perform the cheap checks first.
-        if raw.commands.is_empty() {
+        if config.commands.is_empty() {
             bail!("commands cannot be empty");
-        }
-
-        let mut config = Self {
-            commands: HashMap::new(),
-            timing: raw.timing,
-        };
-
-        for (command, sequence) in raw.commands {
-            let sequence = parse_sequence(sequence.as_str())
-                .with_context(|| format!("failed to parse {} as a key sequence", sequence))?;
-
-            config.commands.insert(command, sequence);
         }
 
         Ok(config)
     }
 }
 
-// TODO: This could be extended to accept a DSL of sorts...
-fn parse_sequence(sequence: &str) -> Result<Vec<Vk>> {
-    let sequence = sequence
-        .split('+')
-        .filter(|x| !x.is_empty())
-        .collect::<Vec<&str>>();
+fn parse_sequence<'de, D>(deserializer: D) -> Result<HashMap<String, Vec<Vk>>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let mut parsed: HashMap<String, Vec<Vk>> = Default::default();
+    let commands: HashMap<String, String> = Deserialize::deserialize(deserializer)?;
 
-    let mut parsed = Vec::new();
+    for (command, sequence) in commands {
+        let sequence = sequence
+            .split('+')
+            .filter(|x| !x.is_empty())
+            .collect::<Vec<&str>>();
 
-    for key in sequence {
-        parsed.push(parse_key(key).with_context(|| format!("failed to parse {} as a key", key))?);
+        parsed.insert(
+            command,
+            Deserialize::deserialize(sequence.into_deserializer())?,
+        );
     }
 
     Ok(parsed)
-}
-
-fn parse_key(key: &str) -> Result<Vk> {
-    let key = match key {
-        "Alt" => Vk::Alt,
-        "Backspace" => Vk::Backspace,
-        "CapsLock" => Vk::CapsLock,
-        "Control" => Vk::Control,
-        "Delete" => Vk::Delete,
-        "DownArrow" => Vk::DownArrow,
-        "End" => Vk::End,
-        "Escape" => Vk::Escape,
-        "F1" => Vk::F1,
-        "F2" => Vk::F2,
-        "F3" => Vk::F3,
-        "F4" => Vk::F4,
-        "F5" => Vk::F5,
-        "F6" => Vk::F6,
-        "F7" => Vk::F7,
-        "F8" => Vk::F8,
-        "F9" => Vk::F9,
-        "F10" => Vk::F10,
-        "F11" => Vk::F11,
-        "F12" => Vk::F12,
-        "Home" => Vk::Home,
-        "LeftArrow" => Vk::LeftArrow,
-        "Windows" => Vk::LeftWin,
-        "PageDown" => Vk::PageDown,
-        "PageUp" => Vk::PageUp,
-        "Enter" => Vk::Enter,
-        "RightArrow" => Vk::RightArrow,
-        "Shift" => Vk::Shift,
-        "Space" => Vk::Space,
-        "Tab" => Vk::Tab,
-        "UpArrow" => Vk::UpArrow,
-        "0" => Vk::_0,
-        "1" => Vk::_1,
-        "2" => Vk::_2,
-        "3" => Vk::_3,
-        "4" => Vk::_4,
-        "5" => Vk::_5,
-        "6" => Vk::_6,
-        "7" => Vk::_7,
-        "8" => Vk::_8,
-        "9" => Vk::_9,
-        "A" => Vk::A,
-        "B" => Vk::B,
-        "C" => Vk::C,
-        "D" => Vk::D,
-        "E" => Vk::E,
-        "F" => Vk::F,
-        "G" => Vk::G,
-        "H" => Vk::H,
-        "I" => Vk::I,
-        "J" => Vk::J,
-        "K" => Vk::K,
-        "L" => Vk::L,
-        "M" => Vk::M,
-        "N" => Vk::N,
-        "O" => Vk::O,
-        "P" => Vk::P,
-        "Q" => Vk::Q,
-        "R" => Vk::R,
-        "S" => Vk::S,
-        "T" => Vk::T,
-        "U" => Vk::U,
-        "V" => Vk::V,
-        "W" => Vk::W,
-        "X" => Vk::X,
-        "Y" => Vk::Y,
-        "Z" => Vk::Z,
-        _ => {
-            // TODO: Support lowercase keys in Config
-            bail!("unknown key: {}", key);
-        }
-    };
-
-    Ok(key)
 }
