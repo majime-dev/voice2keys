aa1a6207cb12bc55e59f3eeeb48d35a01c522e05
Akutō Gushiken <akuto@majime.dev>
2023-03-19 12:48:08 +0100

    build: add a manifest file

--- build.rs
+++ build.rs
@@ -2,5 +2,9 @@ extern crate winresource;
 
 fn main() {
     let mut res = winresource::WindowsResource::new();
-    res.set_icon("resources/v2k.ico").compile().unwrap();
+
+    res.set_icon("resources/v2k.ico")
+        .set_manifest_file("resources/manifest.xml")
+        .compile()
+        .unwrap();
 }
